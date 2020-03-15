0cf85c250d6a63f0e03dc1786c361bdbc022ca08
Ademir Ferreira <ademir.ae86@gmail.com>
2020-03-15 00:04:46 -0300

    Get and display song lyrics

--- script.js
+++ script.js
@@ -41,6 +41,21 @@ async function getMoreSongs(url) {
   showData(data);
 }
 
+// Get lyrics for song
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '</br>');
+
+  result.innerHTML = `
+    <h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <span>${lyrics}</span>;
+  `;
+
+  more.innerHTML = '';
+}
+
 form.addEventListener('submit', e => {
   e.preventDefault();
 
@@ -52,3 +67,14 @@ form.addEventListener('submit', e => {
     searchSong(searchTerm);
   }
 });
+
+result.addEventListener('click', e => {
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === 'BUTTON') {
+    const artist = clickedEl.getAttribute('data-artist');
+    const songTitle = clickedEl.getAttribute('data-songTitle');
+
+    getLyrics(artist, songTitle);
+  }
+})
